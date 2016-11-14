3375b7b96d41aa4bde9d196a52455beca3c0029b
Weems <mdweems@iastate.edu>
2016-11-14 11:10:37 -0600

    Pull pictures from GoPro

--- arduinoCloseLoop/linux_server/get_pic.c
+++ arduinoCloseLoop/linux_server/get_pic.c
@@ -1,59 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
- 
-#include <curl/curl.h>
- 
-static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
-{
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
-}
- 
-int main(int argc, char *argv[])
-{
-  CURL *curl_handle;
-  static const char *pagefilename = "page.out";
-  FILE *pagefile;
- 
-  if(argc < 2) {
-    printf("Usage: %s <URL>\n", argv[0]);
-    return 1;
-  }
- 
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* init the curl session */ 
-  curl_handle = curl_easy_init();
- 
-  /* set URL to get here */ 
-  curl_easy_setopt(curl_handle, CURLOPT_URL, argv[1]);
- 
-  /* Switch on full protocol/debug output while testing */ 
-  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
- 
-  /* disable progress meter, set to 0L to enable and disable debug output */ 
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
- 
-  /* send all data to this function  */ 
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
- 
-  /* open the file */ 
-  pagefile = fopen(pagefilename, "wb");
-  if(pagefile) {
- 
-    /* write the page body to this file handle */ 
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
- 
-    /* get it! */ 
-    curl_easy_perform(curl_handle);
- 
-    /* close the header file */ 
-    fclose(pagefile);
-  }
- 
-  /* cleanup curl stuff */ 
-  curl_easy_cleanup(curl_handle);
- 
-  return 0;
+#include <iostream> 
+#include <curl/curl.h> 
+#include <stdio.h> 
+
+using namespace std;
+
+char url[] = "http://10.5.5.9:8080/video/DCIM/GOPR0100/";
+char tempfile[] = "temp.JPG";
+
+/*
+ * Usage: ./get_pic <integer> <photoname>
+ */
+int main()
+{ 
+  CURL *image; 
+  CURLcode imgresult; 
+  FILE *fp = NULL; 
+
+  int imgNo = (int) strtol(argv[1], (char **)NULL, 10);
+  char nFile[] = strcat(argv[1], ".JPG");
+
+  image = curl_easy_init(); 
+  if( image ){ 
+    // Open file
+    execve("/bin/cp/", tempfile, nFile);
+    fp = fopen(nFile, "wb"); 
+    if( fp == NULL ) cout << "File cannot be opened"; 
+
+    // construct image url
+    char imgFn[] = strcat(argv[2], ".JPG");
+    char imgUrl[] = strcat(url, imgFn);
+    
+    // fetch image
+    curl_easy_setopt(image, CURLOPT_URL, imgUrl); 
+    curl_easy_setopt(image, CURLOPT_WRITEFUNCTION, NULL); 
+    curl_easy_setopt(image, CURLOPT_WRITEDATA, fp); 
+
+
+    // Grab image 
+    imgresult = curl_easy_perform(image); 
+    if( imgresult ){ 
+        cout << "Cannot grab the image!\n"; 
+    } 
+  } 
+
+  // Clean up the resources 
+  curl_easy_cleanup(image); 
+  // Close the file 
+  fclose(fp); 
+  return 0; 
 }
