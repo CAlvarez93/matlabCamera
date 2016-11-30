9ca0bb00047ea5b90c1875a4b5529c3b8e73d901
Weems <mdweems@iastate.edu>
2016-11-30 16:43:11 -0600

    updated run for angledCameraMethod

--- angledCameraMethod/run.m
+++ angledCameraMethod/run.m
@@ -2,9 +2,9 @@ clc
 clear all
 close all
 
-% output file
+%% output file
 oid = fopen('results.csv', 'wt');
-formatspec = '%d, %d, %d, %d, %d, %d, %d, %d\n';
+formatspec = '%0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f\n';
 fprintf(oid, 'Actual Height, Calculated Height, A, Pixel Height, Actual Skew Factor, Calculated Skew Factor, Percent Error, Distance\n');
 
 dirname = 'test_images/';
@@ -13,34 +13,39 @@ label = sprintf('Actual Height, Calculated Height, A, Pixel Height, Actual Skew 
 %label = sprintf('pixel height');
 disp(label);
 
-object_height = [ 37.5 25.75 43.25 ];
+%% set these values
+object_height = 37.5;
+%object_height = 25.75;
+%object_height = 43.25;
 
 fid = fopen('lidar_data.csv');
-C = textscan(fid, '%d %d %s', 'Delimiter', ',');
+C = textscan(fid, '%f %d %s', 'Delimiter', ',');
 rows = size(C{1});
 
 fclose(fid);
 
 for arg=1:rows
-    
+  
   % file
   fname = C{3}{arg};
   image_filename = strcat(dirname,fname);
   
   % distance
   input_dist = C{1}(arg);
-   
+  
   mainScript
   
   % run error checking
-  [sf, pe ] = sfCheck(h, object_height(1), a, cam_height, skew_factor);
+  [sf, pe] = sfCheck(h, object_height, a, cam_height, skew_factor);
   
   % print object height
-  print_out = sprintf('%d, %d, %d, %d, %d, %d, %d, %d', object_height(1), obj_height, a, h, sf, skew_factor, pe, input_dist);
+  print_out = sprintf('%0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f, %0.4f', object_height(1), obj_height, a, h, sf, skew_factor, pe, input_dist);
 %   print_out = sprintf('%d',h);
-  %disp(print_out);
+  disp(print_out);
 
-  fprintf(oid, formatspec, object_height(1), obj_height, a, h, sf, skew_factor, pe, input_dist);
+  fprintf(oid, formatspec, object_height, obj_height, a, h, sf, skew_factor, pe, input_dist);
+  pause(5);
+  close all
 end
 fclose(oid);
-quit;
+%quit;
